ff8a9e3f3cb9fff0f3828e59150c94a04cb1df0d
Koki Matsuoka <matsuoka.koki0808@gmail.com>
2023-04-08 04:33:41 -0400

    Added user Tarou Yamada

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,5 +16,11 @@ class UserSeeder extends Seeder
     {
         // create 20 users
         User::factory(20)->create();
+        // create new user with email example@example.com
+        User::factory()->create([
+            'email' => 'example@example.com',
+            'name' => 'Tarou Yamada',
+            'password' => bcrypt('password'),
+        ]);
     }
 }
