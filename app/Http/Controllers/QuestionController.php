8bcdd64f631786ad59007c3742d13d08fb67b08a
Koki Matsuoka <matsuoka.koki0808@gmail.com>
2023-04-08 04:14:47 -0400

    installed breeze

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //
+    // return view
+    public function index()
+    {
+        return view('questions.index')->with([
+            'questions' => Question::all()
+        ]);
+    }
 }
